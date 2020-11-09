9533666bde2c7e6936439719808e621e660457fd
Jyotirmoy <bravo68web@gmail.com>
2020-11-09 15:56:38 +0530

    PWA Fixes with Workbox

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/sw.js").then(
+    navigator.serviceWorker.register("./sw.js").then(
       function (registration) {
         // Registration was successful
         console.log(
